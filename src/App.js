3c192a4dabb0fa2442626349ca9d4cdcca8529f8
Akshit Kumar <akshitgupta150@gmail.com>
2022-05-03 10:13:27 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick1 = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleClick2 = () => {
+    setCounter(counter - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "300%",
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        top: "-15%",
+      }}
+    >
+      Simple Counter App
+      <div
+        style={{
+          fontSize: "120%",
+          position: "relative",
+          top: "10vh",
+        }}
+      >
+        {counter}
+      </div>
+      <div className="buttons">
+        <button
+          style={{
+            fontSize: "60%",
+            position: "relative",
+            top: "20vh",
+            marginRight: "5px",
+            backgroundColor: "white",
+            borderRadius: "10%",
+            color: "black",
+          }}
+          onClick={handleClick1}
         >
-          Learn React
-        </a>
-      </header>
+          +1
+        </button>
+        <button
+          style={{
+            fontSize: "60%",
+            position: "relative",
+            top: "20vh",
+            marginLeft: "5px",
+            backgroundColor: "white",
+            borderRadius: "10%",
+            color: "black",
+          }}
+          onClick={handleClick2}
+        >
+          -1
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
